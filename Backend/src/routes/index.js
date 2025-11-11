b0ee4180110cdd9875c6fd19ee011546c236c934
Pedro Reimberg <pedro.reimberg.oliveira@gmail.com>
2025-11-10 21:12:59 -0300

    Implementacao do CRUD de organizacoes seguindo as regras de negocio

--- Backend/src/routes/index.js
+++ Backend/src/routes/index.js
@@ -3,10 +3,12 @@ const router = express.Router();
 
 const exampleRoutes = require('./example.routes');
 const usuarioRoutes = require('./usuario.routes');
+const organizacaoRoutes = require('./organizacao.routes');
 
 // Todas as rotas em 'example.routes.js' serão prefixadas com /example
 router.use('/example', exampleRoutes);
 router.use('/usuarios', usuarioRoutes);
+router.use('/organizacoes', organizacaoRoutes);
 
 // Adicione mais rotas aqui
 // router.use('/user', userRoutes);
